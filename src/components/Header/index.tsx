5e48be3c1c610e5fc39a6b51b5ff80c3492de5d5
Arthur Lopes <arthurlopescontact@gmail.com>
2025-02-20 20:34:41 -0300

    feat: create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { Container, InputSearchContainer } from "./styles";
+import logo from '../../assets/images/logo.svg';
+
+
+export function Header() {
+  return (
+    <Container>
+      <img src={logo} alt='MyContacts' width='201' />
+      <InputSearchContainer>
+        <input placeholder="Pesquisar contato..." />
+      </InputSearchContainer>
+    </Container>
+  );
+}
